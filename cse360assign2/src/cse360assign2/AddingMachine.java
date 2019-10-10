75dc2d577d288d993b2f5086f2be32898c05ea6e
Raida Khan <rskhan3@asu.edu>
2019-10-09 19:27:52 -0700

    Initial commit of Assignment 2 of CSE360.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,74 @@
+package cse360assign2;
+
+/**
+*
+* @author Raida Khan
+* @classID 85141
+* @assignmentnumber 2
+* 
+*                   This file contains the AddingMachine class with one constructor and 5 methods, 
+*                   including a toString() method, with the purpose of performing addition/subtraction 
+*                   operations. At this time, most of the methods are empty.
+* 
+* 
+*/
+
+/**
+ * AddingMachine class contains one unparameterized constructor and 5 additional
+ * methods, including the toString() method
+ *
+ */
+
+public class AddingMachine {
+
+	private int total;
+
+	/**
+	 * Default class constructor takes in no parameters and initializes the private
+	 * variable 'total'
+	 *
+	 */
+
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * getTotal method which returns an integer total
+	 *@return integer value for total
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/**
+	 * add(int x) method that takes in an integer value and adds it to the total.
+	 * does not return anything
+	 */
+	public void add(int value) {
+
+	}
+
+	/**
+	 * subtract(int x) method that takes in an integer value and subtracts it from
+	 * the total. does not return anything
+	 */
+	public void subtract(int value) {
+
+	}
+
+	/**
+	 * toString() method that returns a string value
+	 *@return a String value
+	 */
+	public String toString() {
+		return "";
+	}
+
+	/**
+	 * clear() method that clears the total
+	 *
+	 */
+	public void clear() {
+	}
+}
